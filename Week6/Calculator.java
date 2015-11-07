a83f471af319183ade0fde64b80c5a85672ba91d
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-07 13:12:58 +0000

    Week 6 and day 5 adjustments

--- Week6/Calculator.java
+++ Week6/Calculator.java
@@ -5,4 +5,29 @@ public class Calculator
 		int answer = d1+d2;
 		return answer;
 	}
+
+	public int subtract(int d1, int d2)
+	{
+		int answer = d1-d2;
+		return answer;
+	}
+
+	public int multiply(int d1, int d2)
+	{
+		int answer = d1*d2;
+		return answer;
+	}
+
+	public double divide(int d1, int d2)
+	{
+		double answer = ((double)d1)/((double)d2);
+		return answer;
+	}
+	
+	public int modulus(int d1, int d2)
+	{
+		int answer = d1%d2;
+		return answer;
+	}
+
 }
--- Week6/MyScript.java
+++ Week6/MyScript.java
@@ -5,5 +5,21 @@ public class MyScript
 		Calculator c = new Calculator();
 		int sum = c.add(12,34);
 		System.out.println(sum);
+
+		sum = c.subtract(12,34);
+		System.out.println(sum);
+
+		sum = c.multiply(12,34);
+		System.out.println(sum);
+
+		double sum2 = c.divide(4,2);
+		System.out.println(sum2);
+
+		sum = c.subtract(12,34);
+		System.out.println(sum);
+
+		sum = c.modulus(4,3);
+		System.out.println(sum);
 	}
+
 }
