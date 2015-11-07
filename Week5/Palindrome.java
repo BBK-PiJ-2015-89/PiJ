8aefa6748290e4b0682e566a4c6238d13e5af88e
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-07 19:31:04 +0000

    Palindrome Recursive example

---
+++ Week5/Palindrome.java
@@ -0,0 +1,45 @@
+public class Palindrome
+{
+	public static void main(String[] args)
+{
+	System.out.println("This programme can check if you enter a Palindrome");
+	Palindrome p = new Palindrome();
+	System.out.println("Please enter a word or sentance below which you think might be one, we'll tell you if it is or not.");
+	String entered = System.console().readLine();
+	String converted = p.reverse(entered);
+	if(entered.equals(converted))
+	{
+		System.out.println("Your word/sentance was a Palindrome!");
+	}
+	else
+	{
+		System.out.println("Your word/sentance was not a Palindrome!");
+	}
+}
+
+public String reverse (String ent)
+{
+	int length = ent.length();
+	int i = length - 1;
+	if (length==0||length==1)
+			{
+				return ent;
+			}
+	String reverse = rev(ent, i);
+	return reverse;
+
+}
+
+String temp ="";
+public String rev (String ent, int i)
+{
+
+	if (i<0){
+		return temp;
+	}
+	temp = temp + ent.charAt(i);
+	rev(ent, i-1);
+	return temp;
+}
+}
+
