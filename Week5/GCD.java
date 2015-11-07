35e4a78d02e243cab48b1f2877b06f1261afa4a7
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-07 22:36:42 +0000

    changed returns to assigns instead, maybe not right, need to check with tutor.

--- Week5/GCD.java
+++ Week5/GCD.java
@@ -4,50 +4,57 @@ public class GCD
 	{
 		GCD g = new GCD();
 		System.out.println("Hello, this programme will tell us the two numbers:");
-		int a = 10;
-		int b = 8;
+		int a = 16;
+		int b = 3;
 		int i = 0;
-		int s = 0;
-		int sum = g.gcd(s, i,a,b);
-		System.out.println(sum);
+		g.s = 0;
+		g.gcd(i,a,b);
+		System.out.println(g.s);
 
 	}
 
-	public int gcd (int s, int i, int a, int b)
-	{
-		if (a>b && a%b==0)
+		public void gcd (int i, int a, int b)
 		{
-			return b;
-		}
-		else if (b>a && b%a==0)
-		{
-			return a;
-		}
-		else if (a>b){
-			i = b;
-		}
-		else
-		{
-			i = a;
+			if (a>b && a%b==0)
+			{
+				s = b;
+			}
+			else if (b>a && b%a==0)
+			{
+				s = a;
+			}
+			else if (a>b)
+			{
+				i = b;
+				recgcd(i, a, b);
+			}
+			else
+			{
+				i = a;
+				recgcd(i, a, b);
+			}
 		}
 
-	int result = recgcd(s, i, a, b);
-	return result;
-	}
-
-	public int recgcd (int s, int i, int a, int b)
-	{
-		if (i<1)
+int s = 0;
+		public void recgcd (int i, int a, int b)
 		{
-			return i;
+			if (i<2)
+			{
+				s=i;
+			}
+			else
+			{
+			int sum1 = a%i;
+			int sum2 = b%i;
+			if (sum1==0 && sum2==0)
+			{
+				s = i;
+			}
+			else
+			{
+				recgcd(i-1, a, b);
+			}
+				
 		}
-		int sum1 = a%i;
-		int sum2 = b%i;
-		if (sum1==0 && sum2==0)
-		{
-			s = i;
-		}
-		recgcd(s, i-1, a, b);
-		return s;
 	}
 }
