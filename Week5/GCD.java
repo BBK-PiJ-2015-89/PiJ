4caa8acba1c2697332a39207ecd2ecad155a8105
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-08 21:03:31 +0000

    improved code for GCD

--- Week5/GCD.java
+++ Week5/GCD.java
@@ -3,58 +3,24 @@ public class GCD
 	public static void main(String[] args)
 	{
 		GCD g = new GCD();
-		System.out.println("Hello, this programme will tell us the two numbers:");
-		int a = 16;
-		int b = 3;
-		int i = 0;
-		g.s = 0;
-		g.gcd(i,a,b);
-		System.out.println(g.s);
+		System.out.println("Hello, this programme will tell us the GCD of two numbers:");
+		int a = 70;
+		int b = 73157;
+		int i = a;
+		int s = g.gcd(i,a,b);
+		System.out.println(s);
 
 	}
 
-		public void gcd (int i, int a, int b)
+	public int gcd (int i, int a, int b)
+	{
+		if (a%i==0 && b%i==0)
 		{
-			if (a>b && a%b==0)
-			{
-				s = b;
-			}
-			else if (b>a && b%a==0)
-			{
-				s = a;
-			}
-			else if (a>b)
-			{
-				i = b;
-				recgcd(i, a, b);
-			}
-			else
-			{
-				i = a;
-				recgcd(i, a, b);
-			}
+			return i;
 		}
-
-int s = 0;
-		public void recgcd (int i, int a, int b)
+		else
 		{
-			if (i<2)
-			{
-				s=i;
-			}
-			else
-			{
-			int sum1 = a%i;
-			int sum2 = b%i;
-			if (sum1==0 && sum2==0)
-			{
-				s = i;
-			}
-			else
-			{
-				recgcd(i-1, a, b);
-			}
-				
+			return gcd(i-1, a, b);
 		}
 	}
 }
