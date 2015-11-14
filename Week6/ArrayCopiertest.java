1030cd37494a9028dc9b7319e17304ec84f29afb
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-14 17:40:56 +0000

    Array copier test added

---
+++ Week6/ArrayCopiertest.java
@@ -0,0 +1,12 @@
+public class ArrayCopiertest
+{
+	public static void main(String[] args)
+	{
+		int[] ids1 = new int[4];
+		int[] ids2 = new int[4];
+		int[] ids3 = new int[2];
+		System.out.println(ids1.length);
+		System.out.println(ids2.length);
+		System.out.println(ids3.length);
+	}
+}
