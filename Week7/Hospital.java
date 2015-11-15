be3f2643784117b9e2b1f8b89f313779e6b62543
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-15 16:20:29 +0000

    Linked list, works fine, but need to add 1st and last one to be deleted method

---
+++ Week7/Hospital.java
@@ -0,0 +1,23 @@
+public class Hospital
+{
+public static void main(String[] args)
+	{
+		Patient patient1 = new Patient("Graeme", 12, "Rabies");
+		patient1.addPatient(new Patient("Dan", 22, "Flu"));
+		patient1.addPatient(new Patient("Phil", 21, "Broken leg"));
+		patient1.addPatient(new Patient("Mark", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Jess", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Alice", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Harry", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Sparky", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Fluffy", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Samuel", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Terry", 17, "Tummy bug"));
+		patient1.addPatient(new Patient("Simon", 17, "Tummy bug"));
+		//Patient patient2 = new Patient("Jake", 32, "large case of boils");
+		//patient1.addPatient(patient2);
+		patient1.printPatient();
+		patient1.deletePatient("Harry");
+		patient1.printPatient();
+	}
+}
