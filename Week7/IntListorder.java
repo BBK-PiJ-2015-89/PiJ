766cb053d8e2d5e9587547287826169126aed6ea
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2015-11-22 16:05:43 +0000

    first attempt at ordered list

---
+++ Week7/IntListorder.java
@@ -0,0 +1,56 @@
+public class IntListorder
+{
+	private String name;
+	private IntValue head;
+
+	public IntListorder(String name)
+	{
+		this.name = name;
+		this.head = null;
+	}
+
+	public void arraySetter(int [] name)
+	{
+	for (int i=0; i<name.length; i++)
+	{
+		this.addValue(new IntValue(name[i]));
+	}
+	}
+	
+
+	public void printValue() 
+	{
+		IntValue element = head;
+		System.out.println("Values in: " + this.name);
+
+		while (element != null) 
+		{
+			element.print();
+			element = element.getNextValue();
+		}
+	}
+
+	public void addValue(IntValue number) 
+	{
+		if (head == null) 
+		{
+			head = number;
+		} 
+		else if (head.getValue()>number.getValue())
+		{
+			number.setNextValue(head);
+			head = number;
+		} 
+		else
+		{
+			IntValue element = head;
+
+			while (element.getNextValue().getValue()<number.getValue() && element.getNextValue()!=null)
+			{
+				element = element.getNextValue();
+			}
+			number.setNextValue(element.getNextValue());
+			element.setNextValue(number);
+		}
+	}
+}
---
+++ Week7/ListUtilitiesorder.java
@@ -0,0 +1,16 @@
+public class ListUtilitiesorder
+{
+	
+	public static void main(String[] args)
+	{
+		IntListorder uch = new IntListorder("List One");
+		uch.addValue(new IntValue(1));
+		
+		IntListorder arraytype = new IntListorder("Array List");
+		int[] a = {1,3,5,7,8,10,28,76,102,101,65,72};
+		arraytype.arraySetter(a);
+		uch.printValue();
+		arraytype.printValue();
+	}
+	
+}
