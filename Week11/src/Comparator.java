92541224542d47a4b9b78f9420fa935173e65e41
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-01-04 19:43:51 +0000

    inclass example updated.

---
+++ Week11/src/Comparator.java
@@ -0,0 +1,19 @@
+public class Comparator<R extends Comparable<R> >{
+
+    public R getMax(R n, R m){
+
+   /*     if (n > m){
+            return n;
+        } else{
+            return m;
+        }*/
+        return (n.compareTo(m) > 0 ? n : m); //if result is above 0 return n else return m
+    }
+
+    public static void main(String[] args) {
+        Comparator c = new Comparator();
+        System.out.println(c.getMax(3,4));
+        System.out.println(c.getMax(3.1, 4.1));
+        System.out.println(c.getMax("10", "20"));
+    }
+}
