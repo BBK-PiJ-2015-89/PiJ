5af40cad1929f5c18ccbf901ed6dd2ccf7142d31
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-02-01 20:34:52 +0000

    Extra vesion with manual set up

--- Week15/ProgrammeRead10.java
+++ Week15/ProgrammeRead10.java
@@ -13,18 +13,28 @@ public class ProgrammeRead10 {
     public static void main(String[] args) {
         int total = 0;
         int counter = 1;
-        while(counter <11){
+        int top = 0;
+        while (top == 0) {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please enter number of numbers you'd like to enter");
+                top = input.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("That's not an int");
+            }
+        }
+        while (counter <= top) {
             try {
                 Scanner input = new Scanner(System.in);
                 System.out.println("Please enter int number " + counter);
                 total = total + input.nextInt();
-                counter ++;
+                counter++;
             } catch (InputMismatchException ex) {
                 System.out.println("That's not an int");
             }
         }
 
-        System.out.println("Average: " + total / 10);
+        System.out.println("Average: " + total / top);
     }
 
 
