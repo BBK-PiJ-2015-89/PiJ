2cc6c88e12623f9898cba20134b82f42101f40f6
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-02-15 18:55:17 +0000

    Week 17 inital commit

--- Week16/Cp.java
+++ Week16/Cp.java
@@ -18,36 +18,39 @@ public class Cp {
         }
 
     }
-    public void launch(String source, String destination){
+    public void launch(String source, String destination) {
         File sourceFile = new File(source);
         File destinationFile = new File(destination);
-
-        if (destinationFile.exists()){
-            System.out.println("Do you want to overwrite " + destination + " - Y/N?: ");
-            if(! new Scanner(System.in).nextLine().toUpperCase().equals("Y")) {
-                System.out.println("Program Terminated, no further action to complete")
-            }else{
-                BufferedReader in = null;
-                PrintWriter out = null;
-                try{
-                    in =  new BufferedReader(new FileReader(sourceFile));
-                    out = new PrintWriter(destinationFile);
-                    String line;
-                    while ((line = in.readLine()) != null){
-                        out.println(line);
-                    }
-                    System.out.println("Finished copying to destination");
-                }catch (FileNotFoundException ex){
-                    System.out.println("File not found");
-                }catch (IOException ex){
-                    ex.printStackTrace();
-                }finally {
-                    closeReader(in);
-                    if (out != null){
-                        out.close();
+        if (sourceFile.exists()) {
+            if (destinationFile.exists()) {
+                System.out.println("Do you want to overwrite " + destination + " - Y/N?: ");
+                if (!new Scanner(System.in).nextLine().toUpperCase().equals("Y")) {
+                    System.out.println("Program Terminated, no further action to complete");
+                } else {
+                    BufferedReader in = null;
+                    PrintWriter out = null;
+                    try {
+                        in = new BufferedReader(new FileReader(sourceFile));
+                        out = new PrintWriter(destinationFile);
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            out.println(line);
+                        }
+                        System.out.println("Finished copying to destination");
+                    } catch (FileNotFoundException ex) {
+                        System.out.println("File not found");
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    } finally {
+                        closeReader(in);
+                        if (out != null) {
+                            out.close();
+                        }
                     }
                 }
             }
+        } else{
+            System.out.println("Source file does not exist");
         }
     }
 
