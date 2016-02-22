e361a1daa98825f8ead8041313a76b9d40bba2af
BBK-PiJ-2015-89 <gwilki01@mail.bbk>
2016-02-22 20:44:31 +0000

    question 3 set up

--- Week18/ExecutorImpl.java
+++ Week18/ExecutorImpl.java
@@ -1,7 +1,39 @@
 package Week18;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.Executor;
+
 /**
  * Created by graemewilkinson on 22/02/16.
  */
 public class ExecutorImpl {
+    final Queue<Runnable> tasks = new ArrayDeque<Runnable>();
+    final Executor executor;
+    Runnable active;
+
+    ExecutorImpl(Executor executor) {
+        this.executor = executor;
+    }
+
+    public synchronized void execute(final Runnable r) {
+        tasks.offer(new Runnable() {
+            public void run() {
+                try {
+                    r.run();
+                } finally {
+                    scheduleNext();
+                }
+            }
+        });
+        if (active == null) {
+            scheduleNext();
+        }
+    }
+
+    protected synchronized void scheduleNext() {
+        if ((active = tasks.poll()) != null) {
+            executor.execute(active);
+        }
+    }
 }
